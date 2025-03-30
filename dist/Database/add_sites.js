26f9523a9a6c2443608ccdc0a4be292d3206ca9e
karan-mishra-sudo <mishrazack69@gmail.com>
2025-03-30 11:31:12 +0530

    Delete dist directory

--- dist/Database/add_sites.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = add_sites;
-const model_1 = require("./model");
-async function add_sites(new_site, key) {
-    await model_1.UserModel.updateOne({ id: key }, { $push: { "services.static_site": new_site } });
-}
--- dist/Database/find_user.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = find_user;
-const model_js_1 = require("./model.js");
-async function find_user(user) {
-    return await model_js_1.UserModel.findOne({ id: user.id });
-}
