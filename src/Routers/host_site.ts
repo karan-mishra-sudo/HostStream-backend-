6144ec39a5425cdffbf852ccad2c8cad5623cebd
karan-mishra-sudo <mishrazack69@gmail.com>
2025-04-07 20:14:33 +0530

    Update host_site.ts

--- src/Routers/host_site.ts
+++ src/Routers/host_site.ts
@@ -17,7 +17,7 @@ export default host_site.post('/', upload.array('files', 20), (req, res) => {
         website_name: req.body.websiteName,
         site_folder: `/uploads/${locations.user_folder_loactions}/${locations.user_site_loactions}`,
         route: `/${route}`,
-        URL: `http://localhost:88/${route}`,
+        URL: `https://hs-qsij.onrender.com/${route}`,
         Date: new Date().toLocaleDateString()
     }
     Database.add_sites(new_site, req.body.id).then(() => {
