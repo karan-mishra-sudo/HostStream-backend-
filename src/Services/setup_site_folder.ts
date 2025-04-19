15b3578437934c44163db3ae9b99d252f5eccaf1
karan-mishra-sudo <mishrazack69@gmail.com>
2025-04-19 20:34:14 +0530

    Delete src directory

--- src/Services/setup_site_folder.ts
+++
@@ -1,5 +0,0 @@
-import { locations } from "../Data/data";
-import generate_name from "./generate_name.js";
-export default function setup_site_folder() {
-    locations.user_site_loactions=generate_name();
-}
