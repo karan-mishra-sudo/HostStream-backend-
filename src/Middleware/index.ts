0a468789a36c2261ba6befe63d92228e8413c0e7
karan-mishra-sudo <mishrazack69@gmail.com>
2025-04-25 10:13:26 +0530

    Delete src directory

--- src/Middleware/index.ts
+++
@@ -1,4 +0,0 @@
-import get_site_route from "./get_site_route";
-export default {
-    get_site_route
-}
--- src/Redis/remove_item.ts
+++
@@ -1,7 +0,0 @@
-import { redis_item_type } from "../Data/types";
-import { get_redis } from "../Data/data";
-export default async function remove_items(item_key:redis_item_type) {
-    const client= get_redis();
-    const itemString = JSON.stringify(item_key);
-    return (await client)?.LREM('WebList',1,itemString);
-}
